54efa93c2d8d951f214b248a5b8f7b6f5c8ee396
Kirill Tankov <nolixqwerty@mail.ru>
2023-11-11 21:26:22 +0300

    Внёс условные конструкции if и else

---
+++ with User.py
@@ -0,0 +1,74 @@
+# vot = int(input("Сколько лет твоей маме: "))
+# vot2 = int(input("Сколько лет тебе: "))
+# print("Возраст когда мамка тебя родила:", vot - vot2)
+
+'''
+if 5 == 6:
+    print("yes")
+else:
+    print("иди нахуй")
+'''  # if и else
+
+# isGood = False
+
+'''if isGood:
+    print("Пользователь счастлив")
+elif vot2 == 14:
+    print("Пользователь на стадии развития")
+else:
+    print("Пользователь не счастлив")''' # if elif else
+
+'''isYou = True
+while isYou:
+    if input("Введите данные: ") == "stop":
+        isYou = False''' # while бесконечный цикл
+
+'''language = "russian"
+daytime = "morning"
+if language == "english":
+    if daytime == "morning":
+        print("Good morning")
+    else:
+        print("Good evening")
+else:
+    if daytime == "morning":
+        print("Доброе утро")
+    else:
+        print("Добрый вечер")''' # if else (внутри)
+
+'''number = 1
+
+while number < 5:
+    print(f"number = {number}")
+    number += 1
+print("Работа программы завершена")''' # while
+
+'''def test_func(debil):
+    klei = "Четыре"
+
+    if klei == "Пять":
+        print('ты дебил?')
+    else:
+        print(debil)
+
+test_func("Придурок сука")''' # def with if and else
+
+'''def print_person(name, age):
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+
+
+print_person("Tom", 37)
+''' # Ещё одна функция
+
+'''def print_person(name, age, company="Microsoft"):
+    print(f"Name: {name}  Age: {age}  Company: {company}")
+
+
+print_person("Tom", company="JetBrains", age= 24)
+print_person("Кирилл", age= 21, company= "Web-Cult")'''
+
+
+
+
+
