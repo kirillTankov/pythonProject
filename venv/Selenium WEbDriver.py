09c369bea5bcc213ff14a7a59869b1af678a5746
Kirill Tankov <nolixqwerty@mail.ru>
2024-02-28 23:58:18 +0300

    Проверка авторизованных пользователей

--- venv/Selenium WEbDriver.py
+++ venv/Selenium WEbDriver.py
@@ -1,40 +1,55 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options)
 
+class AuthTest:
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=self.options)
+        self.driver.maximize_window()
 
-driver.maximize_window()
-driver.get("https://www.saucedemo.com/")
+    def login(self, username, password):
+        self.driver.get("https://www.saucedemo.com/")
+        eleLogin = self.driver.find_element(By.ID, "user-name")
+        elePassword = self.driver.find_element(By.ID, "password")
+        eleButton = self.driver.find_element(By.ID, "login-button")
+        eleLogin.send_keys(username)
+        elePassword.send_keys(password)
+        eleButton.click()
+        # Проверяем успешность входа
+        if "inventory" in self.driver.current_url:
+            print(f"Пользователь {username} успешно вошел.")
+            return True
+        else:
+            print(f"Ошибка входа для пользователя {username}.")
+            return False
 
-eleLogin = driver.find_element(By.ID, "user-name")
-elePassword = driver.find_element(By.ID, "password")
-eleButton = driver.find_element(By.ID, "login-button")
+    def logout(self):
+        eleMenu = self.driver.find_element(By.ID, "react-burger-menu-btn")
+        eleMenu.click()
+        eleLogout = self.driver.find_element(By.CSS_SELECTOR, "#logout_sidebar_link")
+        eleLogout.click()
 
-eleLogin.send_keys("standard_user")
-elePassword.send_keys("secret_sauce")
+    def clear_fields(self):
+        eleLogin = self.driver.find_element(By.ID, "user-name")
+        elePassword = self.driver.find_element(By.ID, "password")
+        eleLogin.clear()
+        elePassword.clear()
 
-eleButton.click()
+auth_test = AuthTest()
 
-eleMenu = driver.find_element(By.ID, "react-burger-menu-btn")
+# Примеры тестирования каждого пользователя
+users = [
+    ("standard_user", "secret_sauce"),
+    ("locked_out_user", "secret_sauce"),
+    ("problem_user", "secret_sauce"),
+    ("performance_glitch_user", "secret_sauce"),
+    ("error_user", "secret_sauce"),
+    ("visual_user", "secret_sauce")
+]
 
-eleMenu.cloick()
-
-eleLogout = driver.find_element(By.ID, "logout_sidebar_link")
-
-eleLogout.click()
-
-eleLogin.send_keys("locked_out_user")
-elePassword.send_keys("secret_sauce")
-
-eleButton.click()
-
-'''eleAddCart = driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack")
-
-eleAddCart.click()''' # Добавлен товара в корзину
-
-
-#driver.set_page_load_timeout(30)
-# driver.close()
+for user in users:
+    success = auth_test.login(user[0], user[1])
+    if not success:
+        auth_test.clear_fields()  # Очищаем поля ввода
