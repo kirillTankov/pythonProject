00c852f8e32c778bf47d701e8199864232342524
Kirill Tankov <nolixqwerty@mail.ru>
2024-02-28 22:07:54 +0300

    Первый автотест на селениум

---
+++ venv/Selenium WEbDriver.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+
+driver.maximize_window()
+driver.get("https://www.saucedemo.com/")
+
+eleLogin = driver.find_element(By.ID, "user-name")
+elePassword = driver.find_element(By.ID, "password")
+eleButton = driver.find_element(By.ID, "login-button")
+
+eleLogin.send_keys("standard_user")
+elePassword.send_keys("secret_sauce")
+
+eleButton.click()
+
+#driver.set_page_load_timeout(30)
+#driver.close()
